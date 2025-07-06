648a7cedf9d5eeb37a3752b51565198d7e821bef
itscsp <chethanspoojary1@gmail.com>
2025-07-06 18:12:25 +0530

    Learning Layout flex box and building simple todo app

--- App.js
+++ App.js
@@ -1,39 +1,65 @@
 
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  const onPressLearnMore = () => {
-    console.log('Button Pressed');  
-  }
-  return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.dummyText}>Welcome to React Native!</Text>
+  const [enteredGoalTxt, setEnteredGoalTxt] = useState('');
+  const [courceGoals, setCourceGoals] = useState([]);
 
-      </View>
-      <Text style={styles.dummyText}>hello world</Text>
-      <Button
-        onPress={onPressLearnMore}
-        title="Learn More"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
-      />
+  function goalInputHandler(input) { // This function expects an event object, not just the text.
+    setEnteredGoalTxt(input)
+  }
+  
+  function addGoalHandler() {
+    setCourceGoals((prevGoals) => [...prevGoals, enteredGoalTxt])
+    setEnteredGoalTxt('')
+  }
+
+  return (
+    <View style={styles.appContainer}>
+     <View style={styles.inputContianer}>hello
+      <TextInput style={styles.textInput} placeholder="Your cource goal!" value={enteredGoalTxt} onChangeText={goalInputHandler} />
+      <Button title="Add Goal" onPress={addGoalHandler} />
+     </View>
+     <View style={styles.goalContainer}>
+      {courceGoals.map((goal) => {
+        return <Text>{goal}</Text>
+      })}
+
+      {courceGoals.length < 0 &&
+        <Text>List of goals...</Text>}
+     </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+  appContainer: {
+    flex:1,
+    paddingTop:50,
+    paddingHorizontal:16,
   },
 
-  dummyText: {
-    margin:16,
-    padding:16,
-    borderWidth: 2,
-    borderColor: 'blue',
+  inputContianer: {
+    flex:1,
+    flexDirection:'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom:24,
+    borderBottomWidth:1,
+    borderBottomColor:'#cccccc'
+  },
+
+  textInput: {
+    borderWidth:1,
+    borderColor:'#cccccc',
+    width:'70%',
+    marginRight:8,
+    padding:8,
+  },
+
+  goalContainer: {
+    flex:4
   }
 });
