367603cca841267c918390bfe2632ba6393b4e4c
itscsp <chethanspoojary1@gmail.com>
2025-07-18 09:16:26 +0530

    fix: adjust padding in Header and TabBar components for improved layout

--- NewsApp/components/UI/Header.tsx
+++ NewsApp/components/UI/Header.tsx
@@ -34,6 +34,7 @@ const style = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
+    paddingTop:10
   },
   userImg: {
     width: 40,
--- NewsApp/components/UI/TabBar.tsx
+++ NewsApp/components/UI/TabBar.tsx
@@ -88,7 +88,7 @@ const styles = StyleSheet.create({
   tabbar: {
     flexDirection: 'row',
     paddingTop: 16,
-    paddingBottom:40,
+    paddingBottom:20,
     backgroundColor: Colors.white,
   }
 })
