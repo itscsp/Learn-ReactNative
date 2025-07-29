186bd8076163ea5b39acd320219e48142afbfbad
itscsp <chethanspoojary1@gmail.com>
2025-07-29 17:49:50 +0530

    feat: Learning state managment

--- 04-RN-Navigation-MealsApp/screen/FavoritesScreen.js
+++ 04-RN-Navigation-MealsApp/screen/FavoritesScreen.js
@@ -1,13 +1,17 @@
 import { View, Text, StyleSheet } from "react-native";
-import React, { useContext } from "react";
-import { FavoriteContext } from "../store/context/favorites-context";
+// import React, { useContext } from "react";
+// import { FavoriteContext } from "../store/context/favorites-context";
 import MealList from "../components/MealsList/MealList";
 import { MEALS } from "../data/dummy-data";
-
+import { useSelector } from "react-redux";
 export default function FavoritesScreen() {
-  const FvtMealsCtx = useContext(FavoriteContext);
+  
+  // const FvtMealsCtx = useContext(FavoriteContext);
+  const favoriteMeals = useSelector(state => state.favoriteMeals.ids);
 
-  const FvtMeals = MEALS.filter((meal) => FvtMealsCtx.ids.includes(meal.id));
+  // const FvtMeals = MEALS.filter((meal) => FvtMealsCtx.ids.includes(meal.id));
+  const FvtMeals = MEALS.filter((meal) => favoriteMeals.ids.includes(meal.id));
+
 
   if (FvtMeals.length === 0) {
     return (
